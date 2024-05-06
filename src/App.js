19d4e8d2a86d71bb2f086416cbe15b2177c4ec67
Caesar <ignatiuscaesar16@gmail.com>
2024-05-06 10:30:12 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <h1>Welcome to TimeCapsule</h1>
         <p>Store your memories and access them anytime in the future.</p>
+        <p>Your No1 App.</p>
       </header>
     </div>
   );
